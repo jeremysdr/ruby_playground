0b7cfadfcafea7857fcf335fdac4860660d3a48f
sarcinal <sarcinal@friendster.com>
2011-02-22 14:40:59 +0800

    Initial cmmit of Sheila's practice file

---
+++ practice.rb
@@ -0,0 +1,28 @@
+ #Class Calculator
+
+ 
+  
+  puts "add, subtract, multiply or divide"
+  command = gets.chomp
+
+ if command == 'add'
+ puts "enter una at pangalawang numero"
+ una = gets.chomp
+ pangalawa = gets. chomp
+ puts una.to_f + pangalawa.to_f
+ end
+ 
+
+ if command == 'subtract'
+  puts "enter una at pangalawang numero"
+  una = gets.chomp
+  pangalawa = gets.chomp
+  puts una.to_f - pangalawa.to_f
+  end
+
+  if command == 'multiply'
+  puts "enter una at pangalawang numero"
+  una = gets.chomp
+  pangalawa = gets.chomp
+  puts una.to_f * pangalawa.to_f
+  end
